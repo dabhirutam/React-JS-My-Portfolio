90a22b6dd80a69a7e615b65e8eae7fd5614e9525
dabhirutam <dabhirutam34@gmail.com>
2025-03-11 06:20:29 -0700

    React-JS-My-Portfolio

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,11 @@
 import { Container, Row, Col, Button } from "react-bootstrap";
-import profileImage from "../images/person.png"
+import profileImage from "../images/person.png";
+import pattern from "../images/banner-pattern.png";
 
 const Banner = () => {
+
     return (
-        <section className="d-flex align-items-center p-5 overflow-x-hidden" style={{height: '100vh'}}>
+        <section className="d-flex align-items-center p-5 overflow-x-hidden" style={{ height: '100vh' }}>
             <Container fluid>
                 <Row className="align-items-center">
                     <Col md={6} className="text-center text-md-start">
@@ -20,6 +22,9 @@ const Banner = () => {
                         <div className="profile-container">
                             <div className="custom-frame ms-auto p-3">
                                 <img src={profileImage} alt="Zemo Plakson" className="profile-image" />
+                                <div style={{ width: '120%', aspectRatio: '1/1' }} className="position-absolute translate-middle top-50 start-50 z-n1">
+                                    <img style={{ width: '100%', aspectRatio: '1/1', animation: '50s linear infinite patternRotate' }} src={pattern} alt=""/>
+                                </div>
                             </div>
                         </div>
                     </Col>
